7df5adae5a6ab11115be9163ac3bd7bf11cf0682
Anarkenker <Anarkenker222@gmail.com>
2025-10-06 22:37:09 +0800

    add HIKSDK + paraloader + HIKdriver

---
+++ Visionlib/include/video/hikdriver.h
@@ -0,0 +1,26 @@
+#ifndef OPENRM_VIDEO_HIKDRIVER_H
+#define OPENRM_VIDEO_HIKDRIVER_H
+
+#include <iostream>
+#include <X11/Xlib.h> 
+#include <assert.h>  
+#include "math.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h> 
+#include "MvCameraControl.h" // 海康相机SDK头文件，在HIK下的include目录
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <pthread.h>
+#include "datamanager/parameter_loader.h"
+
+extern cv::Mat HIKimage;
+extern std::mutex HIKframemtx;
+
+int HIKcamtask();
+
+#endif
